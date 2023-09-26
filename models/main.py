82366775a7c38235f04f1c3f784a682eab5db7d1
wildanam <ahmadwildan.am@gmail.com>
2023-09-26 19:42:43 +0700

    memperpanjang tombol upload, photo, dan generate

--- models/main.py
+++ models/main.py
@@ -2,6 +2,6 @@ from models.wajah import Wajah
 from models.kacamata import Kacamata
 
 class Model:
-    def __init__():
-        wajah_model = Wajah()
-        Kacamata_model = Kacamata()
+    def __init__(self):
+        self.wajah_model = Wajah()
+        self.Kacamata_model = Kacamata()
--- views/select.py
+++ views/select.py
@@ -17,11 +17,11 @@ class SelectView(Frame):
         self.image_container = self.canvas_image.create_image(0, 0, image=self.initial_photo, anchor=tk.NW)
         self.canvas_image.grid(row=1, column=0, padx=10, pady=10)
 
-        self.upload_btn = Button(self, text="upload")
+        self.upload_btn = Button(self, text="upload", width=30)
         self.upload_btn.grid(row=2, column=0, padx=10, pady=10)
 
-        self.photo_btn = Button(self, text="photo")
+        self.photo_btn = Button(self, text="photo", width=30)
         self.photo_btn.grid(row=3, column=0, padx=10, pady=10)
 
-        self.generate_btn = Button(self, text="Generate")
+        self.generate_btn = Button(self, text="Generate", width=30)
         self.generate_btn.grid(row=4, column=0, padx=10, pady=10)
