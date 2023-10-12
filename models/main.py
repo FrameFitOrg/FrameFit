46c6eca6b9fcd26231872a5d526b3b1ce48d63b5
wildanam <ahmadwildan.am@gmail.com>
2023-10-12 11:36:21 +0700

    memperbaiki bug di file main.py di folder models

--- models/main.py
+++ models/main.py
@@ -2,6 +2,6 @@ from models.wajah import Wajah
 from models.kacamata import Kacamata
 
 class Model:
-    def __init__():
+    def __init__(self):
         wajah_model = Wajah()
         Kacamata_model = Kacamata()
